dbd362f01d47981cf4e8ca90471a0237e4c4a7f0
Dulguun <dulguun@Bilguuns-MacBook-Pro-2.local>
2022-10-13 21:17:27 +0800

    version 2.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Squere from './squire';
 const App = () => {
-
+  let a = ['blue', 'yellow', 'yellow', 'yellow', 'aqua', 'aqua', 'aqua', 'red']
+  
+  return (
+    <div className='row'>
+      {
+        a.map((a, i) => {
+          return <Squere color={a} key={i} />
+        })
+      }
+    </div>
+  )
 }
 
 export default App;
---
+++ src/squire.js
@@ -0,0 +1,10 @@
+import "./css.css";
+
+const Squere = ({ color }) => {
+    return (
+        <div className={`squire ${color}`}>
+        </div>
+    )
+}
+
+export default Squere;
